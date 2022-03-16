2654cea4d06d638d7feae5f082d93dac33412b09
Martin Mönnigmann <martin.moennigmann@rub.de>
2022-03-16 15:51:46 +0100

    deleted unused file

--- teaching/step005-test-closed-loop-with-integrator/arduino/closed-loop-step-response/record_data_for_matlab/get_angle_to_gravity.h
+++
@@ -1,7 +0,0 @@
-#ifndef GET_ANGLE_TO_GRAVITY_H_
-#define GET_ANGLE_TO_GRAVITY_H_
-
-#include "get_angle_to_gravity.h"
-double get_angle_to_gravity(uint8_t the_mpu_address);
-
-#endif // GET_ANGLE_TO_GRAVITY_H_
