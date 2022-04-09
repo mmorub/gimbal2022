6bce46d06eb95b0fad3a46db64e0c390817c9114
Martin Mönnigmann <martin.moennigmann@rub.de>
2022-04-09 19:13:08 +0200

    added missing ';'

--- lab/step002-open-loop-step-response/arduino/record_open_loop_step_response_data_for_matlab/MyGB2208Motor.h
+++ lab/step002-open-loop-step-response/arduino/record_open_loop_step_response_data_for_matlab/MyGB2208Motor.h
@@ -9,5 +9,5 @@ const uint8_t pole_pair_number= 14;
 const float pole_pair_factor= (float) pole_pair_number/2.0; // float for convenience
 const float Uqmax= 5.0; // V, max voltage to be used in open loop
 const float driver_voltage_power_supply= 7.5; // V, voltage of motor driver power supply
-const float motor_velocity_limit= 500/360*2*3.1416 // 500deg/s
+const float motor_velocity_limit= 500/360*2*3.1416; // 500deg/s
 #endif // MYGB2208MOTOR_H_
