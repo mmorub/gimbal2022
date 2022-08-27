aa29f0352b2a662161f00710aa359677967c23a1
Martin Mönnigmann <martin.moennigmann@rub.de>
2022-08-27 10:16:38 +0200

    set pole_pair_number to 7 (was 14), deleted helper pole_pair_factor

--- lab/step002-open-loop-step-response/arduino/record_open_loop_step_response_data_for_matlab/MyGB2208Motor.h
+++ lab/step002-open-loop-step-response/arduino/record_open_loop_step_response_data_for_matlab/MyGB2208Motor.h
@@ -5,8 +5,7 @@
 #ifndef MYGB2208MOTOR_H_
 #define MYGB2208MOTOR_H_
 
-const uint8_t pole_pair_number= 14; 
-const float pole_pair_factor= (float) pole_pair_number/2.0; // float for convenience
+const uint8_t pole_pair_number= 7; 
 const float Uqmax= 5.0; // V, max voltage to be used in open loop
 const float driver_voltage_power_supply= 7.5; // V, voltage of motor driver power supply
 const float motor_velocity_limit= 500/360*2*3.1416; // 500deg/s
