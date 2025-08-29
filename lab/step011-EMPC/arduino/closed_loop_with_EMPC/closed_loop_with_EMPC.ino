8af6f08c712cb0f0bf9078a5b057efcd4f040bfc
Sebastian Leonow <sebastian.leonow@ruhr-uni-bochum.de>
2025-08-29 15:06:43 +0200

    added step 11: EMPC

---
+++ lab/step011-EMPC/arduino/closed_loop_with_EMPC/closed_loop_with_EMPC.ino
@@ -0,0 +1,169 @@
+/*  
+ * For registers like PWR_MGMT_1 of the MPU6050 see
+ * Datasheets/Invensense2013-MPU-6050-Register-Map. "MPU_" is prepended 
+ * to all registers. 
+ */
+ 
+#include "MyMPU6050Sensor.h"
+#include "MyGB2208Motor.h"
+#include "get_gyro_y.h"
+#include <Wire.h>
+#include <SimpleFOC.h> 
+#include "ectrl.h"
+#include "empcSequential.h"
+
+// Variables needed for polling gyrometers.
+const int16_t gyro_y_raw_offset= 17.0;
+const float delta_t= 0.005; // seconds
+const float deg_to_rad= 3.1416/180.0; 
+int16_t gyro_y_raw;
+
+// BLDC motor & driver instance
+BLDCMotor motor = BLDCMotor(pole_pair_number);
+BLDCDriver3PWM driver = BLDCDriver3PWM(11, 10, 9, 6, 5, 3); // BLDCDriver3PWM(pwmA, pwmB, pwmC, Enable(optional));
+
+float electr_angle;  
+
+// Variables needed to ensure approximate realtime in loop().
+uint32_t newtime, oldtime, timestep, integrated_jitter, time0;  
+
+float phi, omega;  // angle and angular velocity of camera
+// delete this: float r, e, e_int, e_diff, e_last; // reference angle error, integrated error, d/dt error
+float electrical_angle; 
+
+float u;
+float X[3]  = {0.0, 0.0, 0.0};
+static float u_opt[MPT_RANGE];
+
+void loop(){
+
+  const uint16_t num_time_steps= 200;         // in between angular steps
+  
+  /**
+   * Needed to ensure approximate realtime, do not alter.
+   * Wait until current timestep is over.
+   */
+  newtime= micros();
+  while (newtime < oldtime + timestep){
+    newtime= micros(); 
+  }
+  /* 
+   * end real time control 
+   */
+
+  // Measure omega with gyrometer
+  gyro_y_raw= get_gyro_y(MPU_ADDRESS); 
+  gyro_y_raw= gyro_y_raw- gyro_y_raw_offset;
+
+  /** 
+   * Update states and e
+   */
+
+  omega= gyro_y_raw* gyroRawTo1000dps; // omega= x2
+  phi= phi+ omega* delta_t;    // integrates omega, phi= x1
+
+  X[1] = phi;
+  X[2] = omega;
+
+  X[0] = X[0] + (0.0 - X[1]);
+  
+  empcSequential(X,u_opt);
+
+  u=-u_opt[0];
+  electr_angle= (u+15.0)* deg_to_rad* pole_pair_factor; 
+  motor.setPhaseVoltage(Uqmax, 0, electr_angle);    
+ 
+  // output for matlab
+  Serial.print(phi);
+  Serial.print(',');
+  Serial.print(omega);
+  Serial.print(',');
+  Serial.println(u);
+
+  /**
+   * Needed for realtime control, do not alter
+   */
+  // Integrate sampling time error, recall newtime-oldtime should ideally be 5000,
+  // but may be a little longer, see next comment.
+  integrated_jitter = integrated_jitter- 5000 + newtime - oldtime;
+
+  // Reduce current sampling time by one step (4e-6 seconds) 
+  // if jitter is > 0. Recall jitter < 0 cannot occur,
+  // since while-loop used above to delay next poll may only take a little too long,
+  // but not too short a time. 
+  if (integrated_jitter> 0) {
+    timestep= 4996;
+  } else {
+    timestep= 5000;
+  }
+  oldtime= newtime;
+  /* 
+   * end real time control  
+   */
+}
+
+void setup() {
+
+  /**
+   * Motor driver configuration.
+   */
+  // power supply voltage [V]
+  driver.voltage_power_supply = driver_voltage_power_supply;
+  driver.init();
+  // link the motor and the driver
+  motor.linkDriver(&driver); // motor instance required, because setPhaseVoltage is
+
+  // set motor constraints and init motor
+  motor.voltage_limit = Uqmax;   // [V]
+  motor.velocity_limit = 500/360*2*3.1416; // 500deg/s 
+  // init motor hardware
+  motor.init();
+
+  // setPhaseVoltage(Uq, Ud, electrical_angle
+  electrical_angle= 0; // rad
+  motor.setPhaseVoltage(Uqmax, 0, electrical_angle);    
+  delay(1000); 
+  
+  /**
+   * Sensor configuration (MPU6050)
+   */
+  // turn off low pass filter, set resolution to +/-1000 deg/s.  
+  Wire.begin(); // start i2c
+  Wire.beginTransmission(MPU_ADDRESS); // connect to MPU6050
+  Wire.write(MPU_PWR_MGMT_1); // write zero to PWR_MGMT_1 register 
+  Wire.write(0);              // to wake up MPU6050
+  Wire.endTransmission(false); // send all bytes, false means arduino reserves to be master 
+
+  // connect again and select low pass filter bandwidth and gyro sensitivity
+  Wire.beginTransmission(MPU_ADDRESS); 
+  Wire.write(MPU_CONFIG);     // write zero to MPU_CONFIG register
+  Wire.write(0);              // to select maximum low pass filter bandwidth
+
+  // set gyro resolution 
+  Wire.write(MPU_GYRO_CONFIG);  // write 0x10 to MPU_GYRO_CONFIG register
+  Wire.write(gyro_sensitivity); // to select +/-1000 deg/s sensitivity
+  Wire.endTransmission(true);   // true means arduino let's other devices be master
+
+  // Connect to serial interface; required for Serial.print. 
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10); 
+  }
+
+  /** 
+   * initial conditions and initial reference value for angle phi 
+   */
+  phi= 0.0;   // degrees 
+  omega= 0.0; // degrees per second
+  
+  /**
+   * Variables needed to ensure approximate realtime.
+   */
+  // All times are in microseconds.
+  // Do this at the end of setup.
+  newtime= 0;
+  timestep= 5000; // 5000 microseconds= 5ms, not constant to compensate jitter
+  integrated_jitter = 0; // used to adjust timestep
+  oldtime= micros(); 
+  time0= oldtime; 
+}
---
+++ lab/step011-EMPC/arduino/closed_loop_with_EMPC/get_gyro_y.h
@@ -0,0 +1,6 @@
+#ifndef GET_GYRO_Y_H_
+#define GET_GYRO_Y_H_
+
+float get_gyro_y(uint8_t the_mpu_address);
+
+#endif // GET_GYRO_Y_H_
